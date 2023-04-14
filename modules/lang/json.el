646dc00ebe0fa38bfa004c503b6a3435696a5b02
Robert Deusser <robertjdeusser@gmail.com>
2023-04-14 09:53:16 -0400

    feat: add jqfmt-minify command

--- modules/lang/json.el
+++ modules/lang/json.el
@@ -11,7 +11,12 @@
 (defun jqfmt (start end)
   "Format the current selecton or buffer with jq."
   (interactive "r")
-  (call-process-region start end "jq" t t))
+  (call-process-region start end "jq" t t t))
+
+(defun jqfmt-minify (start end)
+  "Format the current selection or buffer with jq and minify the result."
+  (interactive "r")
+  (call-process-region start end "jq" t t t "-c"))
 
 (provide 'json)
 
