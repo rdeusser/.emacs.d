82fc2d1dd87637f7c17e54fea00c7af51a404997
Robert Deusser <robertjdeusser@gmail.com>
2024-04-01 11:56:30 -0400

    feat: add editorconfig module

---
+++ modules/editorconfig.el
@@ -0,0 +1,17 @@
+;;; editorconfig --- editorconfig configuration.
+
+;;; Commentary:
+
+;;; Functions:
+
+;;; Hooks:
+
+;;; Code:
+
+(use-package editorconfig
+  :config
+  (editorconfig-mode 1))
+
+(provide 'editorconfig)
+
+;;; editorconfig.el ends here
