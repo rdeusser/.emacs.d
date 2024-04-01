31fb4febe5f0319c80bca589d94a363cae7bf6c1
Robert Deusser <robertjdeusser@gmail.com>
2024-04-01 11:57:04 -0400

    feat: use yaml-mode instead of yaml-ts-mode and add pretty function

--- modules/lang/yaml.el
+++ modules/lang/yaml.el
@@ -8,8 +8,10 @@
 
 ;;; Code:
 
-(add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-ts-mode))
+(use-package yaml-mode)
+
+(add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 
 (defun yqfmt (start end)
   "Format the current selecton or buffer with yq."
@@ -18,6 +20,15 @@
   (if (use-region-p)
       (call-process-region start end "yq" t t t)
     (call-process-region (point-min) (point-max) "yq" t t t))
+  (goto-char current-point))
+
+(defun yqfmt-pretty (start end)
+  "Format the current selecton or buffer with yq -P."
+  (interactive "r")
+  (setq current-point (point))
+  (if (use-region-p)
+      (call-process-region start end "yq" t t t)
+    (call-process-region (point-min) (point-max) "yq" t t t "-P"))
   (goto-char current-point))
 
 (provide 'yaml)
